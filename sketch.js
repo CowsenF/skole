a71f0389b81636e3c69954441dbd925bb148f1cd
CowsenF <50503934+CowsenF@users.noreply.github.com>
2021-09-23 20:59:15 +0200

    11.4: Brightness Mirror - p5.js Tutorial

--- sketch.js
+++ sketch.js
@@ -1,23 +1,30 @@
+var video;
+
+var vScale = 16;
+
 function setup() {
-  createCanvas(320, 240);
+  createCanvas(640, 480);
   pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(width / vScale, height / vScale);
 }
 
 function draw() {
   background(51);
-
-  loadPicels();
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      var index = (x + y * width)*4;
-      pixels[index+0] = x;
-      pixels[index+1] = random(255);
-      pixels[index+2] = y;
-      pixels[index+3] = 255;
-      
-    }    
+  video.loadPixels();
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y * video.width)) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+      var bright = (r + g + b) / 3;
+      var w = map(bright, 0, 255, 0, vScale);
+      noStroke();
+      fill(255);
+      rectMode(CENTER);
+      rect(x * vScale, y * vScale, w, w);
+    }
   }
-  updatePixels();
-
 
 }
